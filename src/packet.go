9016b88b90d34c93930161b9fec25f7755353a8a
Tomasen <tomasen@gmail.com>
2016-03-12 19:41:29 +0800

    pass the test that send multiple packet at same time

--- src/packet.go
+++ src/packet.go
@@ -25,6 +25,21 @@ type packet struct {
 	Cmd    cmd
 }
 
+func (p packet) Copy() packet {
+    r := packet{
+			Connid:p.Connid,
+			Seqid:p.Seqid,
+			Cmd:p.Cmd,
+		}
+
+		if p.Buf != nil {
+			r.Buf = make([]byte, buffersize)
+			copy(r.Buf, p.Buf)
+		}
+
+		return r
+}
+
 // sendRaw only happens in backend to remote upstream addr
 func (t *trafcacc) sendRaw(p packet) {
 	log.WithFields(log.Fields{
@@ -101,6 +116,17 @@ func (t *trafcacc) sendRaw(p packet) {
 
 // send packed data to backend, only used on front-end
 func (t *trafcacc) sendPkt(p packet) {
+	t.realSendPkt(p)
+	t.realSendPkt(p)
+}
+
+func (t *trafcacc) realSendPkt(p packet) {
+
+	u := t.upool.next()
+
+	u.mux.Lock()
+	defer u.mux.Unlock()
+
 	log.WithFields(log.Fields{
 		"connid": p.Connid,
 		"seqid":  p.Seqid,
@@ -109,56 +135,50 @@ func (t *trafcacc) sendPkt(p packet) {
 		"zdata":  shrinkString(hex.EncodeToString(p.Buf)),
 	}).Debugln(t.roleString(), "sendPkt()")
 
-	u := t.upool.next()
-
-	func() {
-		u.mux.Lock()
-		defer u.mux.Unlock()
-		if u.conn == nil {
-			// dial
-			switch u.proto {
-			case "tcp":
-				conn, err := net.Dial("tcp", u.addr)
-				if err != nil {
-					// reply error and close connection to client
-					log.WithFields(log.Fields{
-						"connid": p.Connid,
-					}).Debugln(t.roleString(), "dial in sendpkt() failed:", err)
-					t.replyRaw(packet{Connid: p.Connid, Cmd: close})
-					return
-				}
-				u.conn = conn
-				u.encoder = gob.NewEncoder(conn)
-				u.decoder = gob.NewDecoder(conn)
-				// build packet reading slaves
-				go func() {
-					rname := "sendpktDecode"
-					routineAdd(rname)
-					defer routineDel(rname)
-
-					defer u.close()
-
-					u.mux.RLock()
-					dec := u.decoder
-					u.mux.RUnlock()
-					for {
-						p := packet{}
-						err := dec.Decode(&p)
-						if err != nil {
-							log.WithFields(log.Fields{
-								"connid": p.Connid,
-								"error":  err,
-							}).Debugln(t.roleString(), "read packet from backend failed")
-							return
-						}
-						t.replyRaw(p)
-					}
-				}()
-			case "udp":
-				// TODO: udp
+	if u.conn == nil {
+		// dial
+		switch u.proto {
+		case "tcp":
+			conn, err := net.Dial("tcp", u.addr)
+			if err != nil {
+				// reply error and close connection to client
+				log.WithFields(log.Fields{
+					"connid": p.Connid,
+				}).Debugln(t.roleString(), "dial in sendpkt() failed:", err)
+				t.replyRaw(packet{Connid: p.Connid, Cmd: close})
+				return
 			}
+			u.conn = conn
+			u.encoder = gob.NewEncoder(conn)
+			u.decoder = gob.NewDecoder(conn)
+			// build packet reading slaves
+			go func() {
+				rname := "sendpktDecode"
+				routineAdd(rname)
+				defer routineDel(rname)
+
+				defer u.close()
+
+				u.mux.RLock()
+				dec := u.decoder
+				u.mux.RUnlock()
+				for {
+					p := packet{}
+					err := dec.Decode(&p)
+					if err != nil {
+						log.WithFields(log.Fields{
+							"connid": p.Connid,
+							"error":  err,
+						}).Debugln(t.roleString(), "read packet from backend failed")
+						return
+					}
+					t.replyRaw(p)
+				}
+			}()
+		case "udp":
+			// TODO: udp
 		}
-	}()
+	}
 
 	err := u.encoder.Encode(&p)
 	if err != nil {
@@ -201,6 +221,23 @@ func (t *trafcacc) replyRaw(p packet) {
 
 // reply Packet only happens in backend to frontend
 func (t *trafcacc) replyPkt(p packet) error {
+	e1 := t.realReplyPkt(p)
+	e2 := t.realReplyPkt(p)
+
+	if e1 != nil && e2 != nil {
+		if e1 != nil {
+			return e1
+		}
+
+		if e2 != nil {
+			return e2
+		}
+	}
+
+	return nil
+}
+
+func (t *trafcacc) realReplyPkt(p packet) error {
 	log.WithFields(log.Fields{
 		"connid": p.Connid,
 		"seqid":  p.Seqid,
